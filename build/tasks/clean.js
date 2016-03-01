d596dbf1cd825af326fc8065528c73c08f3ce520
Simon Rijk <s.rijk@viriciti.com>
2016-03-01 08:39:23 +0100

    By default clean server folder first

--- build/tasks/clean.js
+++ build/tasks/clean.js
@@ -9,16 +9,29 @@ log = require("../lib/log");
 cleanBuildDirectory = require("../lib/clean").cleanBuildDirectory;
 
 module.exports = function(coffeeProjectOptions) {
-  var enabled, options, targetDirectoryPath;
+  var clientDirectoryPath, enabled, options, serverDirectoryPath, targetDirectoryPath;
   options = coffeeProjectOptions.clean;
   enabled = options.enabled;
   targetDirectoryPath = path.resolve(options.targetDirectoryPath);
-  return gulp.task("clean", function(cb) {
+  clientDirectoryPath = path.resolve(options.clientDirectoryPath);
+  serverDirectoryPath = path.resolve(options.serverDirectoryPath);
+  gulp.task("clean:client", function(cb) {
     if (!enabled) {
-      log.info("Skipping clean: Disabled.");
+      log.info("Skipping clean client: Disabled.");
       return cb();
     }
-    log.debug("[clean] Cleaning `" + targetDirectoryPath + "`.");
-    cleanBuildDirectory(targetDirectoryPath, cb);
+    log.debug("[clean] Cleaning client build path `" + clientDirectoryPath + "`.");
+    cleanBuildDirectory(clientDirectoryPath, cb);
+  });
+  gulp.task("clean:server", function(cb) {
+    if (!enabled) {
+      log.info("Skipping clean server: Disabled.");
+      return cb();
+    }
+    log.debug("[clean] Cleaning server build path `" + serverDirectoryPath + "`.");
+    cleanBuildDirectory(serverDirectoryPath, cb);
+  });
+  return gulp.task("clean", ["clean:client", "clean:server"], function(cb) {
+    cb();
   });
 };
--- src/tasks/default.coffee
+++ src/tasks/default.coffee
@@ -2,9 +2,10 @@ gulp        = require "gulp"
 runSequence = require "run-sequence"
 
 module.exports = ->
-
 	gulp.task "default", (cb) ->
 		runSequence [
+			"clean:server"
+		], [
 			"bundle:vendor"
 			"coffee:compile"
 			"copy:compile"
