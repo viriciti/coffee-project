921609ec1820f2f528bb4f764cdd3d99b6983795
swapsCAPS <verstegen.daan@gmail.com>
2018-11-20 17:24:20 +0100

    built

--- build/tasks/coffee/compile.js
+++ build/tasks/coffee/compile.js
@@ -1,4 +1,8 @@
-var gulp, gulpCoffee, gulpSourcemaps, gulpTap, log, path;
+var _, async, gulp, gulpCoffee, gulpSourcemaps, gulpTap, log, path;
+
+_ = require("underscore");
+
+async = require("async");
 
 path = require("path");
 
@@ -13,35 +17,47 @@ gulpTap = require("gulp-tap");
 log = require("../../lib/log");
 
 module.exports = function(coffeeProjectOptions) {
-  var enabled, isProduction, noSourcemaps, options, sourceDirectoryPath, targetDirectoryPath;
+  var enabled, isProduction, noSourcemaps, options;
   options = coffeeProjectOptions.coffee;
   enabled = options.enabled;
-  sourceDirectoryPath = path.resolve(options.sourceDirectoryPath);
-  targetDirectoryPath = path.resolve(options.targetDirectoryPath);
   isProduction = process.env.NODE_ENV === "production";
   noSourcemaps = isProduction ? true : !!options.noSourcemaps;
   return gulp.task("coffee:compile", function(cb) {
-    var coffeeCompiler, s;
+    var sourceDirectoryPath, targetDirectoryPath;
     if (enabled !== true) {
       log.info("Skipping coffee:compile: Disabled.");
       return cb();
     }
-    log.debug("[coffee:compile] Source directory path: `" + sourceDirectoryPath + "`.");
-    log.debug("[coffee:compile] Target directory path: `" + targetDirectoryPath + "`.");
-    coffeeCompiler = gulpCoffee({
-      bare: true
-    });
-    coffeeCompiler.on("error", log.error.bind(log));
-    s = gulp.src(sourceDirectoryPath + "/**/*.coffee").pipe(gulpTap(function(file) {
-      return log.debug("[coffee:compile] Compiling `" + file.path + "`.");
-    }));
-    if (!noSourcemaps) {
-      s = s.pipe(gulpSourcemaps.init());
+    sourceDirectoryPath = options.sourceDirectoryPath;
+    targetDirectoryPath = options.targetDirectoryPath;
+    if (!Array.isArray(sourceDirectoryPath)) {
+      sourceDirectoryPath = [sourceDirectoryPath];
     }
-    s = s.pipe(coffeeCompiler);
-    if (!noSourcemaps) {
-      s = s.pipe(gulpSourcemaps.write());
+    if (!Array.isArray(targetDirectoryPath)) {
+      targetDirectoryPath = [targetDirectoryPath];
     }
-    s.pipe(gulp.dest(targetDirectoryPath)).once("end", cb);
+    async.each(_.zip(sourceDirectoryPath, targetDirectoryPath), function(arg, cb) {
+      var coffeeCompiler, s, source, target;
+      source = arg[0], target = arg[1];
+      source = path.resolve(source);
+      target = path.resolve(target);
+      log.debug("[coffee:compile] Source directory path: `" + source + "`.");
+      log.debug("[coffee:compile] Target directory path: `" + target + "`.");
+      coffeeCompiler = gulpCoffee({
+        bare: true
+      });
+      coffeeCompiler.on("error", log.error.bind(log));
+      s = gulp.src(source + "/**/*.coffee").pipe(gulpTap(function(file) {
+        return log.debug("[coffee:compile] Compiling `" + file.path + "`.");
+      }));
+      if (!noSourcemaps) {
+        s = s.pipe(gulpSourcemaps.init());
+      }
+      s = s.pipe(coffeeCompiler);
+      if (!noSourcemaps) {
+        s = s.pipe(gulpSourcemaps.write());
+      }
+      return s.pipe(gulp.dest(target)).once("end", cb);
+    }, cb);
   });
 };
--- build/tasks/coffee/watch.js
+++ build/tasks/coffee/watch.js
@@ -1,4 +1,8 @@
-var diskWatcher, fs, gulp, gulpCoffee, gulpSourcemaps, log, path;
+var _, async, diskWatcher, fs, gulp, gulpCoffee, gulpSourcemaps, log, path;
+
+async = require("async");
+
+_ = require("underscore");
 
 fs = require("fs");
 
@@ -15,79 +19,92 @@ log = require("../../lib/log");
 diskWatcher = require("../../lib/disk-watcher");
 
 module.exports = function(coffeeProjectOptions) {
-  var enabled, isProduction, noSourcemaps, options, sourceDirectoryPath, targetDirectoryPath, watchEnabled;
+  var enabled, isProduction, noSourcemaps, options, watchEnabled;
   options = coffeeProjectOptions.coffee;
   enabled = options.enabled;
-  sourceDirectoryPath = path.resolve(options.sourceDirectoryPath);
-  targetDirectoryPath = path.resolve(options.targetDirectoryPath);
   watchEnabled = coffeeProjectOptions.watch.enabled;
   isProduction = process.env.NODE_ENV === "production";
   noSourcemaps = isProduction ? true : !!options.noSourcemaps;
   return gulp.task("coffee:watch", function(cb) {
-    var compilePath, removePath, watcher;
+    var sourceDirectoryPath, targetDirectoryPath;
     if (!(enabled && watchEnabled)) {
       log.info("Skipping browserify:watch: Disabled.");
       return cb();
     }
-    log.debug("[coffee:watch] Source directory path: `" + sourceDirectoryPath + "`.");
-    log.debug("[coffee:watch] Target directory path: `" + targetDirectoryPath + "`.");
-    compilePath = function(sourcePath) {
-      var coffeeCompiler, s, sourceDirectory, targetDirectory;
-      coffeeCompiler = gulpCoffee({
-        bare: true
-      });
-      coffeeCompiler.on("error", log.error.bind(log));
-      sourceDirectory = path.dirname(sourcePath);
-      targetDirectory = sourceDirectory.replace(sourceDirectoryPath, targetDirectoryPath);
-      s = gulp.src(sourcePath);
-      if (!noSourcemaps) {
-        s = s.pipe(gulpSourcemaps.init());
-      }
-      s = s.pipe(coffeeCompiler);
-      if (!noSourcemaps) {
-        s = s.pipe(gulpSourcemaps.write());
-      }
-      return s.pipe(gulp.dest(targetDirectory));
-    };
-    removePath = function(sourcePath) {
-      var targetPath;
-      targetPath = sourcePath.replace(sourceDirectoryPath, targetDirectoryPath).replace(".coffee", ".js");
-      return fs.unlink(targetPath, function(error) {
-        if (error) {
-          return log.error(error);
+    sourceDirectoryPath = options.sourceDirectoryPath;
+    targetDirectoryPath = options.targetDirectoryPath;
+    if (!Array.isArray(sourceDirectoryPath)) {
+      sourceDirectoryPath = [sourceDirectoryPath];
+    }
+    if (!Array.isArray(targetDirectoryPath)) {
+      targetDirectoryPath = [targetDirectoryPath];
+    }
+    async.each(_.zip(sourceDirectoryPath, targetDirectoryPath), function(arg, cb) {
+      var compilePath, removePath, source, target, watcher;
+      source = arg[0], target = arg[1];
+      log.debug("[coffee:watch] Source directory path: `" + source + "`.");
+      log.debug("[coffee:watch] Target directory path: `" + target + "`.");
+      source = path.resolve(source);
+      target = path.resolve(target);
+      compilePath = function(sourcePath) {
+        var coffeeCompiler, s, sourceDirectory, targetDirectory;
+        coffeeCompiler = gulpCoffee({
+          bare: true
+        });
+        coffeeCompiler.on("error", log.error.bind(log));
+        sourceDirectory = path.dirname(sourcePath);
+        targetDirectory = sourceDirectory.replace(source, target);
+        s = gulp.src(sourcePath);
+        if (!noSourcemaps) {
+          s = s.pipe(gulpSourcemaps.init());
         }
+        s = s.pipe(coffeeCompiler);
+        if (!noSourcemaps) {
+          s = s.pipe(gulpSourcemaps.write());
+        }
+        return s.pipe(gulp.dest(targetDirectory));
+      };
+      removePath = function(sourcePath) {
+        var targetPath;
+        targetPath = sourcePath.replace(source, target).replace(".coffee", ".js");
+        return fs.unlink(targetPath, function(error) {
+          if (error) {
+            return log.error(error);
+          }
+        });
+      };
+      watcher = diskWatcher(coffeeProjectOptions).src();
+      watcher.on("change", function(filePath) {
+        if (!filePath.match(/\.coffee$/)) {
+          return;
+        }
+        if (!filePath.match(new RegExp(source))) {
+          return;
+        }
+        log.info("[coffee:watch] Compiling `" + filePath + "`.");
+        return compilePath(filePath);
       });
-    };
-    watcher = diskWatcher(coffeeProjectOptions).src();
-    watcher.on("change", function(filePath) {
-      if (!filePath.match(/\.coffee$/)) {
-        return;
-      }
-      if (!filePath.match(new RegExp(sourceDirectoryPath))) {
-        return;
-      }
-      log.info("[coffee:watch] Compiling `" + filePath + "`.");
-      return compilePath(filePath);
-    });
-    watcher.on("add", function(filePath) {
-      if (!filePath.match(/\.coffee$/)) {
-        return;
-      }
-      if (!filePath.match(new RegExp(sourceDirectoryPath))) {
-        return;
-      }
-      log.info("[coffee:watch] Compiling `" + filePath + "`.");
-      return compilePath(filePath);
-    });
-    watcher.on("unlink", function(filePath) {
-      if (!filePath.match(/\.coffee$/)) {
-        return;
-      }
-      if (!filePath.match(new RegExp(sourceDirectoryPath))) {
-        return;
-      }
-      log.info("[coffee:watch] Removing `" + filePath + "`.");
-      return removePath(filePath);
+      watcher.on("add", function(filePath) {
+        if (!filePath.match(/\.coffee$/)) {
+          return;
+        }
+        if (!filePath.match(new RegExp(source))) {
+          return;
+        }
+        log.info("[coffee:watch] Compiling `" + filePath + "`.");
+        return compilePath(filePath);
+      });
+      watcher.on("unlink", function(filePath) {
+        if (!filePath.match(/\.coffee$/)) {
+          return;
+        }
+        if (!filePath.match(new RegExp(source))) {
+          return;
+        }
+        log.info("[coffee:watch] Removing `" + filePath + "`.");
+        return removePath(filePath);
+      });
+      return cb();
     });
   });
 };
